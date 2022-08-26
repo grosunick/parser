08300b535e2a14beea3160a93360c23c24dff253
Николай Гросу <grosunick@yandex-team.ru>
2022-08-26 12:40:09 +0300

    remove package

--- src/packages/worker/Interfaces.ts
+++
@@ -1,7 +0,0 @@
-export type PromiseId = number | string
-export type PromiseJob = Promise<PromiseId>
-export type PromiseMap = {[id: PromiseId]: PromiseJob}
-
-export interface IJob {
-    run(): PromiseJob
-}
